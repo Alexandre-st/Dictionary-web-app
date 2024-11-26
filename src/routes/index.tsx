8b01a1eb124efa10912e9d49fabe64e66ba11019
Alexandre-st <alexandre.saintprix@gmail.com>
2024-11-26 21:51:43 +0100

    Add the base of the project

---
+++ src/routes/index.tsx
@@ -0,0 +1,14 @@
+import { createFileRoute } from '@tanstack/react-router';
+import { css } from '../../styled-system/css';
+
+export const Route = createFileRoute('/')({
+  component: HomeComponent,
+});
+
+function HomeComponent() {
+  return (
+    <div>
+      <div className={css({ fontSize: "2xl", fontWeight: 'bold', backgroundColor: 'red.300' })}>Hello 🐼!</div>
+    </div>
+  );
+}
